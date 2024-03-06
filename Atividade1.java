3a4845eed2ccef4d1ce8c896eb18a24f55651a69
Ericles Silva dos Santos <130414832+eriksntss@users.noreply.github.com>
2024-03-06 17:01:26 -0300

    Update Atividade1.java

--- Atividade1.java
+++ Atividade1.java
@@ -4,18 +4,17 @@ public class Atividade1 {
 
 	public static void main(String[] args) {
 		
-		int INDICE = 13;
-		int SOMA = 0;
-		int K = 0;
+		int INDICE = 13, SOMA = 0, K = 0;
 		
 		while (K < INDICE) {
 			K = K + 1;
 			SOMA = SOMA + K;
-			System.out.println(SOMA);
 		}
 		
+		System.out.println(SOMA);
 		
 
 	}
 
 }
+
